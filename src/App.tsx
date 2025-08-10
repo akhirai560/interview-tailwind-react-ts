6f15d981b942f1756d5d376797f7269c1c4f08d9
akhirai560 <akhileshgzp500@gmail.com>
2025-08-10 17:25:17 +0530

    init 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <div className='text-red-300'>Hi Akhi</div>
+    <div className='text-3xl bg-amber-500'>Hi akhi</div>
     </>
-  )
+  );
 }
 
 export default App
