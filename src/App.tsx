87f0139c88b2f2d308dd41e228f9a65e48cfdb34
akhirai560 <akhileshgzp500@gmail.com>
2025-08-15 20:11:44 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { type ComponentPropsWithoutRef } from 'react';
 import './App.css'
+
+type ButtonVariants = 'primary'| 'secondary' | 'tertiary' |'destructive'|'links';
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+  variant?: ButtonVariants,
+  size?: 'md' | 'lg' | 'xl' | '2xl',
+  
+}
+
+function Button({children, variant='primary', size='md' ,...restProps}: ButtonProps) {
+
+  return <button className={['btn', variant, size, 'm-2'].join(' ')} {...restProps} >{children}</button>
+}
 
 function App() {
   return (
     <>
-    <div className='text-3xl bg-amber-500'>Hi akhi</div>
+    <Button style={{marginTop: '20px'}} variant='primary' > Hello akhilesh</Button>
+    <Button style={{color: 'blue'}} size='lg' variant='secondary' > Hello akhilesh</Button>
+    <Button variant='destructive' > Hello akhilesh</Button>
     </>
   );
 }
